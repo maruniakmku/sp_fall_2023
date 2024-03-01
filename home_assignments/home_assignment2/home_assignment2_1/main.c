0ef822d9d4dbd6c529f7cd639fdd2935831d03c8
Nikita Maruniak <maruniak.mku@gmail.com>
2024-03-01 18:17:10 +0200

    Revert home_assignment2_1/main.c

--- home_assignments/home_assignment2/home_assignment2_1/main.c
+++ home_assignments/home_assignment2/home_assignment2_1/main.c
@@ -1,41 +1,5 @@
-#include <stdio.h>
-
-int checkBrackets(char *line) {
-    char stack[100];
-    int top = -1;
-
-    for (int i = 0; line[i] != '\0'; ++i) {
-        if (line[i] == '(' || line[i] == '[' || line[i] == '{') {
-            stack[++top] = line[i];
-        } else if (line[i] == ')' || line[i] == ']' || line[i] == '}') {
-            if (top == -1 || 
-                (line[i] == ')' && stack[top] != '(') ||
-                (line[i] == ']' && stack[top] != '[') ||
-                (line[i] == '}' && stack[top] != '{')) {
-                return 0; 
-            }
-            --top; 
-        }
-    }
-
-    return top == -1; 
-}
-
-int main() {
-    int N;
-    scanf("%d", &N);
-    getchar(); 
-
-    for (int i = 0; i < N; ++i) {
-        char line[102];
-        fgets(line, 102, cstdio);
-
-        if (checkBrackets(line)) {
-            printf("valid\n");
-        } else {
-            printf("invalid\n");
-        }
-    }
-
+int main()
+{
     return 0;
 }
+
